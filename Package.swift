3312ea478d6a0f62c7e8976750cd2a32996d7605
Naswa Bryna Danikhansa <naswakhansa23@gmail.com>
2026-01-29 10:38:39 +0700

    update package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "RMQClient",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
+        .macOS(.v10_13)
     ],
     products: [
         .library(
@@ -12,24 +13,32 @@ let package = Package(
             targets: ["RMQClient"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.5"),
+        .package(url: "https://github.com/naswadani/JKVValue.git", .branch("spm-support")),
+    ],
     targets: [
         .target(
             name: "RMQClient",
-            dependencies: [],
-            path: ".",
-            exclude: ["RMQClientTests", "RMQClientIntegrationTests"],
-            sources: ["RMQClient"],
-            publicHeadersPath: "RMQClient",
+            dependencies: [
+                "CocoaAsyncSocket",
+                "JKVValue",
+            ],
+            path: "RMQClient",
+            publicHeadersPath: ".", 
             cSettings: [
-                .headerSearchPath("RMQClient"),
-                .define("COCOAPODS", to: "1"),
-                .unsafeFlags(["-fno-objc-arc"], .when(configuration: .debug))
+                .headerSearchPath("."),
             ],
             linkerSettings: [
                 .linkedFramework("Foundation"),
                 .linkedFramework("Security"),
                 .linkedFramework("CFNetwork")
             ]
+        ),
+        .testTarget(
+            name: "RMQClientTests",
+            dependencies: ["RMQClient"],
+            path: "RMQClientTests"
         )
     ],
     cLanguageStandard: .c11
